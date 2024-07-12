d0eb3461eff89261b8e30fd11e983b2eb6725543
minseogo <gominseo1112@gmail.com>
2024-07-12 10:40:15 +0900

    버튼 클릭시 배경색깔 바꾸기 삼향식 이용 증가, 감소

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -3,16 +3,24 @@ import React, {useState, useEffect} from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const bgcolor = ["red", "green", "blue"];
+  const bgcolor = ["red", "green", "blue","pink","yellow"];
+
+  // body태그의 배경색
+  // useEffect(()=>{}, []) -> useeffect의 시작 시점 ( usestate가 초기화 시켜주면 )
+
+  useEffect(()=>{
+    document.body.style.backgroundColor = bgcolor[count];
+  }, [count])
+
 
   return (
     <div className="App">
     <button onClick={()=>{
-      setCount(count + 1); // 값에 대한 연산식만 사용가능 (대입식 사용불가 ex) count++, count=count+1))
+      setCount(count < bgcolor.length -1 ? count + 1 : 0); // 값에 대한 연산식만 사용가능 (대입식 사용불가 ex) count++, count=count+1))
     }}> {count} 증가해줘</button>
 
     <button onClick={()=>{
-      setCount(count - 1); 
+      setCount(count > 0 ? count - 1 : bgcolor.length - 1); 
     }}> {count} 감소해줘</button>
     </div>
   );
